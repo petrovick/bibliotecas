c9914c4353b10b7230c517a8543431bc7007e493
petrovick <petrovick@users.noreply.github.com>
2015-09-17 00:17:37 -0300

    Create formatarNumeros.js

---
+++ js/formatarNumeros.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+	$(this).find('.numeros').each (function() {
+		debugger;
+		Number.prototype.formatMoney = function(c, d, t){
+			var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+			   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+			};
+		var numero = parseFloat(this.innerHTML);
+		var num = (numero).formatMoney(3, ',', '.');
+		this.innerHTML = num;
+	});
+	$(this).find('.valores').each (function() {
+		var numero = Number(this.innerHTML);
+		Number.prototype.formatMoney = function(c, d, t){
+			var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+			   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+			};
+		var num = (numero).formatMoney(3, ',', '.');
+		this.innerHTML = 'R$ ' + num;
+	});
+});
